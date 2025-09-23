48523d8dc6b667e422504ae7ca0595acb7b5a253
sivalokeshyadav <222sivalokeshyadav@gmail.com>
2025-09-24 00:38:51 +0530

    context-api is done

---
+++ src/hooks/usePut.ts
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import axiosInstance from "../services/AxiosService"
+
+const usePut=()=>{
+const [data,setData]=useState(null)
+const [error,setError]=useState(undefined)
+const [loading,setLoading]=useState(false)
+
+
+const putData=async(url:string,payload:any)=>{
+    try{
+setLoading(true)
+const response=await axiosInstance.put(url,payload)
+setData(response.data)
+return response.data
+    }catch(error:any){
+        console.error(error.message)
+        setError(error.message)
+        throw error
+    }finally{
+        setLoading(false)
+    }
+
+
+
+}
+
+return {putData,data,error,loading}
+}
+
+
+export default usePut
