58e76b9ff09b55b2c9833900e4482330c5c3736a
thinklusivetech-users <thinklusive10@gmail.com>
2026-03-04 21:05:42 +0530

    remove localstorage

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// Remove stale localStorage keys left over from an older version of the app
-// that used localStorage instead of sessionStorage for form drafts.
-['DRUG_FORM_DATA', 'DRUG_FORM_STEP', 'sidebarCollapsed'].forEach(key =>
-  localStorage.removeItem(key)
-);
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
